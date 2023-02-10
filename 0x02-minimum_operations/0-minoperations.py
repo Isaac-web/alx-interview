05a4f4da32dbd91639a06651a4bc3767c87bd418
Isaac-web <kanytakiy@gmail.com>
2023-02-10 05:55:01 +0000

    Fix Task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -16,12 +16,12 @@ def minOperations(n):
     H = 1
     copy_all = 0
     paste = 0
-    H_copied = 0
+    copied = 0
     while H < n:
         if n % H == 0:
             copy_all += 1
-            H_copied = H
+            copied = H
         paste += 1
         number_of_operations = copy_all + paste
-        H += H_copied
+        H += copied
     return number_of_operations
