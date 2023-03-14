3426a88f7473ae72b3e24c688f5baf9cac5feab2
Isaac-web <kanytakiy@gmail.com>
2023-03-14 12:32:20 +0000

    Task 0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,35 @@
+const request = require('request');
+
+const parseToJson = (obj) => {
+  try {
+    return JSON.parse(obj);
+  } catch (err) {
+    return null;
+  }
+};
+
+const getMovieCharacter = (characterUrls) => {
+  if (!characterUrls.length) return;
+
+  request.get(characterUrls[0], (err, res, body) => {
+    if (err) throw new Error(err);
+
+    const character = parseToJson(body);
+    if (character) console.log(character.name);
+
+    getMovieCharacter(characterUrls.slice(1));
+  });
+};
+
+const getMovieCharaterUrls = (movieId) => {
+  const url = `https://swapi.dev/api/films/${movieId}`;
+
+  request.get(url, (err, res, body) => {
+    if (err) throw new Error('Failed to make request');
+    const movieCharacters = JSON.parse(body).characters;
+
+    getMovieCharacter(movieCharacters);
+  });
+};
+
+getMovieCharaterUrls(3);
