ad3784c7c56049966e971656de41d55510391d15
Isaac-web <kanytakiy@gmail.com>
2023-04-07 00:37:31 +0000

    Add Module Level Comment

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,51 +1,10 @@
-# #!/usr/bin/python3
-
-
-# def is_prime(num):
-#     """
-#     A helper function that returns True if num is prime, False otherwise.
-#     """
-#     if num < 2:
-#         return False
-#     for i in range(2, int(num**0.5)+1):
-#         if num % i == 0:
-#             return False
-#     return True
-
-
-# def isWinner(x, nums):
-#     """
-#     Determines winner of Prime Game
-#     Args:
-#         x (int): no. of rounds of game
-#         nums (int): upper limit of range for each round
-#     Return:
-#         Name of winner (Maria or Ben) or None if winner cannot be found
-#     """
-#     if x is None or nums is None or x == 0 or nums == []:
-#         return None
-#     Maria = Ben = 0
-#     for i in range(x):
-#         num = nums[i]
-#         primes_count = 0
-#         for j in range(2, num+1):
-#             if is_prime(j) and num % j == 0:
-#                 primes_count += 1
-#         if primes_count % 2 == 0:
-#             Ben += 1
-#         else:
-#             Maria += 1
-#     if Maria > Ben:
-#         return 'Maria'
-#     elif Ben > Maria:
-#         return 'Ben'
-#     return None
-
-
+#!/usr/bin/python3
+"""Prime Game"""
 
 def primes(n):
     """
-    A helper function that returns a list of prime numbers between 1 and n (inclusive).
+    A helper function that returns a list of prime
+    numbers between 1 and n (inclusive).
     """
     primes_list = []
     sieve = [True] * (n + 1)
@@ -59,12 +18,12 @@ def primes(n):
 
 def isWinner(x, nums):
     """
-    Determines winner of Prime Game.
+    Returns winner of Prime Game.
     Args:
-        x (int): number of rounds of game
-        nums (list): upper limit of range for each round
+        x (int): number of rounds
+        nums (list): upper limit of range
     Return:
-        Name of winner (Maria or Ben) or None if winner cannot be found
+        Name of winner or None if winner cannot be found
     """
     if x is None or nums is None or x == 0 or nums == []:
         return None
